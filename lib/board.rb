d095ea8a71493fc31b856e6cf23ef03da821c9db
Elrashid_Elzein <elrashidelzein8@gmail.com>
2019-03-18 23:50:44 -0700

    creates board, places stones,checks moves

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,28 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    x = Array.new(14) {Array.new(2,{})}
-    x.map_with_index {|cup,i| if i != 6 && i != 13}
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) {[]}
+    place_stones
     
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+       if idx != 6 && idx != 13
+      4.times do
+        cup << :stone
+      end
+    end
   end
 
+ 
+
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos >= 13
+    raise "Starting cup is empty" if @cups[start_pos].empty? 
   end
 
   def make_move(start_pos, current_player_name)
